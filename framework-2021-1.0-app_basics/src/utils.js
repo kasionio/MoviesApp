16cee6ea473cef16af9e836d2bc3c4c17c0bbca2
Illia Kasianenko <kasianenko.illia@gmail.com>
2021-06-13 00:00:45 +0300

    add Secure to request

--- framework-2021-1.0-app_basics/src/utils.js
+++ framework-2021-1.0-app_basics/src/utils.js
@@ -1,5 +1,5 @@
 export default function getMoviePoster(poster_path) {
   return {
-    src: `http://image.tmdb.org/t/p/w185/${poster_path}`,
+    src: `https://image.tmdb.org/t/p/w185/${poster_path}`,
   };
 }
